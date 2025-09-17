d97f401f200d31acd4552ce5623c37b90df94dc7
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-09-16 18:36:48 -0600

    Fix: Changed the URL to the correct GitHub Repo

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -1,12 +1,12 @@
 import Link from "next/link";
 
 export default function StudentInfo() {
-  const githubRepo = "https://github.com/jayco01";
+  const githubRepo = "https://github.com/jayco01/cprg306-assignments.git";
 
   return (
     <div className="w-full flex flex-col justify-center items-center">
       <h2>Jay Layco</h2>
-      <Link className="text-font-size-fluid-0" href={githubRepo}>https://github.com/jayco01</Link>
+      <Link className="text-font-size-fluid-0" href={githubRepo}>https://github.com/jayco01/cprg306-assignments.git</Link>
     </div>
   );
 }
