f8593bd34ee648cbd00719ea95c800593e38f6dc
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-09-22 10:04:23 -0600

    Styles: Added Hover effect for items

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -5,10 +5,10 @@ import {useState} from "react";
 export default function Item({ name, quantity, category }) {
     const htmlString = `${name} ${quantity} ${category} `;
     const [isChecked, setChecked] = useState(false);
-    const activeList = "m-2 p-2 bg-custom-green rounded-sm border-2 border-custom-offWhite"
-    const inactiveList = "m-2 p-2 bg-transparent rounded-sm border-2 border-custom-offWhite"
+    const activeList = "bg-custom-green"
+    const inactiveList = "bg-transparent"
   return (
-    <li className={isChecked ? activeList : inactiveList}>
+    <li className={`${isChecked ? activeList : inactiveList} rounded-sm border-2 border-custom-offWhite m-2 p-2 hover:scale-x-105`}>
       <input
       type="checkbox"
       checked={isChecked}
