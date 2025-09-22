883d011fc64079749a009750ee949cb341f413fe
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-09-22 10:09:59 -0600

    Styles: Added label and padding to checkbox for better UX

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -9,12 +9,14 @@ export default function Item({ name, quantity, category }) {
     const inactiveList = "bg-transparent"
   return (
     <li className={`${isChecked ? activeList : inactiveList} rounded-sm border-2 border-custom-offWhite m-2 p-2 hover:scale-x-105`}>
-      <input
-      type="checkbox"
-      checked={isChecked}
-      onChange={() => setChecked(!isChecked)}
-      className="mr-2"
-      />
+      <label className="cursor-pointer p-2">
+        <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={() => setChecked(!isChecked)}
+        className="mr-2"
+        />
+      </label>
       {htmlString}
     </li>
   );
