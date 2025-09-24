512e4cb3fe5e7aa3df7b41612b67465c32b4096c
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-09-24 11:55:43 -0600

    Fix: Ensured that Link in Week 2 opens in a new tab

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -6,7 +6,11 @@ export default function StudentInfo() {
   return (
     <div className="w-full flex flex-col justify-center items-center">
       <h2>Jay Layco</h2>
-      <Link className="text-font-size-fluid-0" href={githubRepo}>https://github.com/jayco01/cprg306-assignments.git</Link>
+      <Link
+        className="text-font-size-fluid-0"
+        target="_blank"
+        href={githubRepo}>https://github.com/jayco01/cprg306-assignments.git
+      </Link>
     </div>
   );
 }
