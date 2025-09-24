38d43cd30d3e8a82148832d86f514866584fec21
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-09-24 09:36:08 -0600

    Refactor: Moved Quantity and Use State to NewItem Component
    I misunderstood the instructions of the assignment

--- app/week-4/new-item.js
+++ app/week-4/new-item.js
@@ -1,5 +1,24 @@
+"use client";
+
+import {useState} from "react";
+
 export default function NewItem() {
+
+  const [quantity, setQuantity] = useState(1);
+
   return (
     <div></div>
   )
+}
+
+
+const increment = (quantity) => {
+  if (quantity >= 20) {
+    setQuantity(quantity + 1);
+  }
+}
+const decrement = (quantity) => {
+  if (quantity <= 1) {
+    setQuantity(quantity - 1);
+  }
 }
--- app/week-4/page.js
+++ app/week-4/page.js
@@ -1,22 +1,11 @@
 "use client";
 
 import NewItem from "@/app/week-4/new-item";
-import {useState} from "react";
 
 export default function Page() {
-  const [quantity, setQuantity] = useState(1);
+
   return (
     <NewItem/>
   )
 }
 
-const increment = (quantity) => {
-  if (quantity >= 20) {
-    setQuantity(quantity + 1);
-  }
-}
-const decrement = (quantity) => {
-  if (quantity <= 1) {
-    setQuantity(quantity - 1);
-  }
-}
