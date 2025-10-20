e9022fd4245af1b0063d8917c98f444776e56c6a
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-10-19 19:00:07 -0600

    Feat: Made it less complex by narrowing it down to 1 ingredient

--- app/week-8/meal-ideas.js
+++ app/week-8/meal-ideas.js
@@ -7,21 +7,34 @@ export default function MealIdeas({items}) {
   const apiBaseUrl = process.env.NEXT_PUBLIC_RECIPE_API_BASE_URL || import.meta?.env?.VITE_RECIPE_API_BASE_URL;
   const apiKey = process.env.NEXT_PUBLIC_RECIPE_API_KEY || import.meta?.env?.VITE_API_KEY;
 
-  const [ingredients, setIngredients] = useState([]);
+  const [recipe, setRecipe] = useState("");
+  const [ingredient, setIngredient] = useState("");
 
-  const handleAddingItem = (item) => {
-    let itemName = item.name.split(/[\s,]+/);
-    setIngredients([...ingredients, itemName[0]]);
+  // const [ingredients, setIngredients] = useState([]);
+  // const [ingredientClass, setIngredientClass] = useState("");
+  //
+  const handleGettingRecipe = (item) => {
+    setIngredient(item.name.split(/[\s,]+/));
+
   }
+
+  const handleApiCall = async (ingredient) => {
+
+  }
+
+  console.log(ingredients);
   return (
     <div className="bg-custom-green rounded-lg flex flex-col p-8">
-      <label for="ingredients"> Select 1 or more ingredients: </label>
-      <select id="ingredients" multiple size="5" className="bg-custom-dark-green text-font-size-fluid-0 p-4 rounded-lg" >
-        <option key="select">Select Ingredients</option>
-        {items.map((item, index) => (
+      <label htmlFor="ingredients"> Select a ingredient: </label>
+      <select id="ingredients" className="bg-custom-dark-green text-font-size-fluid-0 p-4 rounded-lg" >
+        {items.map((item) => (
           <option
-            onClick={() => handleAddingItem(item)}
-            key={item.key}>{item.name}</option>
+            onClick={() => handleGettingRecipe(item)}
+            key={item.key}
+            className={`${ingredients.includes(item) ? "bg-custom-green" : "bg-custom-darker-green"} `}
+          >
+              {item.name}
+          </option>
         ))};
       </select>
     </div>
