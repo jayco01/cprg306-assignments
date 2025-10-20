1e743dbd61191a8ba4fa51163b07ecb801c35de7
Jay Layco <114456003+jayco01@users.noreply.github.com>
2025-10-19 19:54:33 -0600

    Feat: Successfully fetched from the API

--- app/week-8/meal-ideas.js
+++ app/week-8/meal-ideas.js
@@ -3,40 +3,67 @@
 import {useState} from "react";
 
 export default function MealIdeas({items}) {
-  // local testing
   const apiBaseUrl = process.env.NEXT_PUBLIC_RECIPE_API_BASE_URL || import.meta?.env?.VITE_RECIPE_API_BASE_URL;
   const apiKey = process.env.NEXT_PUBLIC_RECIPE_API_KEY || import.meta?.env?.VITE_API_KEY;
 
   const [recipe, setRecipe] = useState("");
-  const [ingredient, setIngredient] = useState("");
+  // const [ingredient, setIngredient] = useState("");
 
-  // const [ingredients, setIngredients] = useState([]);
-  // const [ingredientClass, setIngredientClass] = useState("");
-  //
-  const handleGettingRecipe = (item) => {
-    setIngredient(item.name.split(/[\s,]+/));
 
+  const requestOptions = {
+    method: 'GET',
+    headers: { 'X-Api-Key': '0qNaLVxKR8vwYOBQ6/W3HQ==O5pM8ix5t0Mu5ajT'},
+    contentType: 'application/json'
+  };
+
+  const handleGettingRecipe = async (item) => {
+    console.log("before split: " + item.target.value);
+    let ingredient = item.target.value.split(/[,]+/);
+    ingredient = isolateName(ingredient[0]);
+    console.log("after split: " + ingredient);
+
+    const url = `${apiBaseUrl}${encodeURIComponent(ingredient)}`;
+
+    console.log(url)
+    try {
+      const response = await fetch(url, requestOptions);
+
+      if (!response.ok) {
+        throw new Error(`Could not find ${ingredient}, ${response.status} ${response.statusText}`);
+      }
+      const recipeResponse = await response.json();
+      setRecipe(recipeResponse);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  console.log(recipe);
+
+  function isolateName(str) {
+    str = str.replace(/\p{Emoji}/gu, '')
+    return str.trim();
   }
 
-  const handleApiCall = async (ingredient) => {
-
-  }
-
-  console.log(ingredients);
   return (
     <div className="bg-custom-green rounded-lg flex flex-col p-8">
-      <label htmlFor="ingredients"> Select a ingredient: </label>
-      <select id="ingredients" className="bg-custom-dark-green text-font-size-fluid-0 p-4 rounded-lg" >
-        {items.map((item) => (
-          <option
-            onClick={() => handleGettingRecipe(item)}
-            key={item.key}
-            className={`${ingredients.includes(item) ? "bg-custom-green" : "bg-custom-darker-green"} `}
-          >
+      <div>
+        <select
+          id="ingredients"
+          className="bg-custom-dark-green text-font-size-fluid-0 p-4 rounded-lg"
+          onChange={handleGettingRecipe}
+          defaultValue=""
+        >
+          <option value="" disabled>Select an ingredient</option>
+          {items.map((item) => (
+            <option key={item.key} value={item.name}>
               {item.name}
-          </option>
-        ))};
-      </select>
+            </option>
+          ))}
+        </select>
+      </div>
+      <div>
+
+      </div>
     </div>
   )
 }
